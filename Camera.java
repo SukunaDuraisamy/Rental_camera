562eddc5e7fb703da97036003173e93cdd674f58
SukunaDuraisamy <137320874+SukunaDuraisamy@users.noreply.github.com>
2025-10-10 14:03:16 +0530

    Create Camera.java

---
+++ Camera.java
@@ -0,0 +1,19 @@
+package Miniproject;
+
+public class Camera 
+{
+	public int ID;
+	public String Brand;
+	public String Model;
+	public double Price;
+	public String Status;
+	
+	public Camera(int id,String brand, String model, double price, String status) 
+	{
+		ID = id;
+		Brand = brand;
+		Model = model;
+		Price = price;
+		Status = status;
+	}	
+}
